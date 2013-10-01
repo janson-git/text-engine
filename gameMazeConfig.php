93d7923905ee0e8921115cbfbc992d34764c0744
ivan.janson <ivan.janson@gmail.com>
2013-10-01 18:25:30 +0400

    - add one new room to map: 'magic room'. It has only one enter from north room, and both exits follow to north room.

--- gameMazeConfig.php
+++ gameMazeConfig.php
@@ -7,13 +7,14 @@ return [
             'startRoom' => true,
             'doors' => [
                 'south' => 'southRoom',
+                'north' => 'magicRoom',
             ],
             'description' => "It's a north room of tower. Old prison for political prisoners.",
             'items' => [
                 'magic box' => [
-                    'id' => 'magic box',
+                    'id'          => 'magic box',
                     'description' => 'A little magic box. When you try to shake it - you can hear some noise.',
-                    'type' => 'item',
+                    'type'        => 'item',
                 ],
             ],
         ],
@@ -25,9 +26,25 @@ return [
             'description' => "Nice room with orange walls. Warm wind blows from ventilation on top.",
             'items' => [
                 'sword' => [
-                    'id' => 'sword',
+                    'id'          => 'sword',
                     'description' => 'A little sharp sword.',
-                    'type' => 'weapon',
+                    'type'        => 'weapon',
+                ],
+            ],
+        ],
+
+        'magicRoom' => [
+            'id' => 'magicRoom',
+            'doors' => [
+                'east' => 'northRoom',
+                'west' => 'northRoom',
+            ],
+            'description' => "Dark room. Shadows, craps and bugs here. Smells like magic.",
+            'items' => [
+                'book' => [
+                    'id'          => 'book',
+                    'description' => 'Book. Probably this is magic book.',
+                    'type'        => 'item',
                 ],
             ],
         ],
